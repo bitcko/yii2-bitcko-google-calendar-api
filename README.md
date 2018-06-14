f2e3f91042f88a3cc79af7dba2361955e46f1ddf
Bitcko <m.backer.shehadi@gmail.com>
2018-06-14 13:29:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,144 @@
+Yii2 Bitcko Google Calendar Api Extension
+=========================================
+Yii2 Bitcko Google Calendar Api Extension use to create and delete events from google calendar 
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require bitcko/yii2-bitcko-google-calendar-api:dev-master
+
+```
+
+or add
+
+```
+"bitcko/bitcko/yii2-bitcko-google-calendar-api": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+1. Download client_secret.json file from  [Google Developer Console](https://console.developers.google.com/).
+2. Move the client_secret.json to app/web dir.
+3. Controller example
+
+```php
+<?php
+
+namespace app\controllers;
+use yii\helpers\Url;
+use yii\web\Controller;
+
+use bitcko\googlecalendar\GoogleCalendarApi;
+
+/**
+ * GoogleApi controller.
+ *
+ * @package backend\controllers
+ * @author  Mhmd Backer shehadi (bitcko) <www.bitcko.com>
+
+ */
+class GoogleApiController extends Controller
+{
+
+
+    public function actionAuth(){
+
+        $redirectUrl = Url::to(['/google-api/auth'],true);
+        $calendarId = 'primary';
+        $username="any_name";
+        $googleApi = new GoogleCalendarApi($username,$calendarId,$redirectUrl);
+        if(!$googleApi->checkIfCredentialFileExists()){
+            $googleApi->generateGoogleApiAccessToken();
+        }
+        \Yii::$app->response->data = "Google api authorization done";
+    }
+    public function actionCreateEvent(){
+        $calendarId = 'primary';
+        $username="any_name";
+        $googleApi = new GoogleCalendarApi($username,$calendarId);
+        if($googleApi->checkIfCredentialFileExists()){
+            $event = array(
+                'summary' => 'Google I/O 2018',
+                'location' => '800 Howard St., San Francisco, CA 94103',
+                'description' => 'A chance to hear more about Google\'s developer products.',
+                'start' => array(
+                    'dateTime' => '2018-06-14T09:00:00-07:00',
+                    'timeZone' => 'America/Los_Angeles',
+                ),
+                'end' => array(
+                    'dateTime' => '2018-06-14T17:00:00-07:00',
+                    'timeZone' => 'America/Los_Angeles',
+                ),
+                'recurrence' => array(
+                    'RRULE:FREQ=DAILY;COUNT=2'
+                ),
+                'attendees' => array(
+                    array('email' => 'lpage@example.com'),
+                    array('email' => 'sbrin@example.com'),
+                ),
+                'reminders' => array(
+                    'useDefault' => FALSE,
+                    'overrides' => array(
+                        array('method' => 'email', 'minutes' => 24 * 60),
+                        array('method' => 'popup', 'minutes' => 10),
+                    ),
+                ),
+            );
+
+           $calEvent = $googleApi->createGoogleCalendarEvent($event);
+            \Yii::$app->response->data = "New event added with id: ".$calEvent->getId();
+        }else{
+            return $this->redirect(['auth']);
+        }
+    }
+
+
+    public function actionDeleteEvent(){
+        $calendarId = 'primary';
+        $username="any_name";
+        $googleApi = new GoogleCalendarApi($username,$calendarId);
+        if($googleApi->checkIfCredentialFileExists()){
+            $eventId ='ugj7cmp7b49v99u13tdfc6n0v4' ;
+
+             $googleApi->deleteGoogleCalendarEvent($eventId);
+            \Yii::$app->response->data = "Event deleted";
+        }else{
+            return $this->redirect(['auth']);
+        }
+    }
+
+    public function actionCalendarsList(){
+        $calendarId = 'primary';
+        $username="any_name";
+        $googleApi = new GoogleCalendarApi($username,$calendarId);
+        if($googleApi->checkIfCredentialFileExists()){
+
+
+          $calendars =    $googleApi->calendarList();
+            \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+            \Yii::$app->response->data = $calendars;
+        }else{
+            return $this->redirect(['auth']);
+        }
+    }
+
+}
+
+
+
+```
+
+
+
+
---
+++ src/GoogleCalendarApi.php
@@ -0,0 +1,219 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mhmd Backer Shehadi
+ * Bitcko
+ * http://www.bitcko.com
+ * Date: 6/13/18
+ * Time: 2:38 PM
+ */
+
+namespace bitcko\googlecalendar;
+
+use Google_Client;
+use Google_Service_Calendar;
+use Google_Service_Calendar_Event;
+use yii\helpers\Url;
+use Google_Service_Exception;
+
+class GoogleCalendarApi
+{
+
+    private $username; // username use to create credentials to different users
+    private $calendarId; // calendarId, id of the calendar
+    private $client; // google client auth
+    private $credentialsPath; // path to credentials
+
+
+    /**
+     * Constructor Setup an authorized API client.
+     * @param $pathToClientSecret string
+     * @param $username string
+     * @param $calendarId string
+     * @param $redirectUrl string
+     */
+    public function __construct($username,$calendarId='',$redirectUrl='')
+    {
+        $this->username = $username;
+        $this->calendarId = $calendarId;
+        $this->client = new Google_Client();
+        $this->client->setApplicationName('Yii Google Calendar API');
+        $this->client->setScopes(Google_Service_Calendar::CALENDAR);
+        $this->client->setAuthConfig('client_secret.json');
+        $this->client->setRedirectUri($redirectUrl);
+        $this->client->setAccessType('offline');
+        $cred = 'google_api_tokens/'.$this->username .'_credentials.json';
+        // Load previously authorized credentials from a file.
+        $this->credentialsPath = $this->expandHomeDirectory($cred);
+    }
+
+    /**
+     * generate api accessToken
+     */
+    public function generateGoogleApiAccessToken(){
+
+        if ($this->checkIfCredentialFileExists()) {
+            $accessToken = json_decode(file_get_contents($this->credentialsPath), true);
+        } else {
+            // Request authorization from the user.
+            if(!isset($_GET['code'])){
+                return \Yii::$app->controller->redirect( $this->client->createAuthUrl());
+            }else{
+                $authCode = $_GET['code'];
+                // Exchange authorization code for an access token.
+                $accessToken = $this->client->fetchAccessTokenWithAuthCode($authCode);
+                // Store the credentials to disk.
+                if (!file_exists(dirname($this->credentialsPath))) {
+                    mkdir(dirname($this->credentialsPath), 0700, true);
+                }
+                file_put_contents($this->credentialsPath, json_encode($accessToken));
+            }
+        }
+        $this->client->setAccessToken($accessToken);
+        // Refresh the token if it's expired.
+        if ($this->client->isAccessTokenExpired()) {
+            $this->client->fetchAccessTokenWithRefreshToken($this->client->getRefreshToken());
+            file_put_contents($this->credentialsPath, json_encode($this->client->getAccessToken()));
+        }
+
+    }
+
+    /**
+     * Create Google Calendar Event.
+     * @param object $event .
+     * @return boolean(false) or Google Calendar Event.
+     */
+    public function createGoogleCalendarEvent($event){
+
+        if ($this->checkIfCredentialFileExists() ) {
+            if( $this->is_connected()){
+                try{
+                    $this->checkAccessToken();
+                    $service = new Google_Service_Calendar($this->client);
+                    $calendarId = $this->calendarId;
+                    $event = new Google_Service_Calendar_Event(array(
+                        'summary' => $event['summary'],
+                        'location' => $event['location'],
+                        'description' => $event['description'],
+                        'start' =>$event['start'],
+                        'end' => $event['end'],
+                        'recurrence'=>$event['recurrence'],
+                        'attendees' => $event['attendees'],
+                        'reminders' => $event['reminders']
+                    ));
+                    return  $service->events->insert($calendarId, $event);
+                }catch (Google_Service_Exception $e){
+                    echo $e->getMessage();
+                }
+            }else{
+                return false;
+            }
+        }
+        return false;
+
+    }
+
+    /**
+     * Delete Google Calendar Event.
+     * @param string $eventId .
+     * @return boolean.
+     */
+    public function deleteGoogleCalendarEvent($eventId){
+        if ($this->checkIfCredentialFileExists() ) {
+            if( $this->is_connected()){
+                try{
+                    $this->checkAccessToken();
+                    $service = new Google_Service_Calendar($this->client);
+                    $calendarId = $this->calendarId;
+                    if($eventId and $service->events->get($calendarId, $eventId) ){
+                        $service->events->delete($calendarId, $eventId);
+                        return true;
+                    }
+                }catch (Google_Service_Exception $e){
+                    echo $e->getMessage();
+                }
+            } else{
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * User Calendar Lists (owner calendars), can use in html dropdown list.
+     * @return  array.
+     */
+    public function calendarList(){
+        $calendars = [];
+        if ($this->checkIfCredentialFileExists() ) {
+            if( $this->is_connected()){
+                try {
+                    $this->checkAccessToken();
+                    $service = new Google_Service_Calendar($this->client);
+                    $calendarList = $service->calendarList->listCalendarList();
+                    foreach ($calendarList->getItems() as $calendarListEntry) {
+                        if($calendarListEntry->getAccessRole()=="owner"){
+                            $calendars [$calendarListEntry->getId()] = $calendarListEntry->getSummary();
+                        }
+                    }
+                }catch (Google_Service_Exception $e){
+                    echo $e->getMessage();
+                }
+            }
+        }
+        return $calendars;
+    }
+    /**
+     * Expands the home directory alias '~' to the full path/ path to web dir in yii2 framework.
+     * @param string $path the path to expand.
+     * @return string the expanded path.
+     */
+    private function expandHomeDirectory($path)
+    {
+        $homeDirectory = getenv('HOME');
+        if (empty($homeDirectory)) {
+            $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+        }
+        return str_replace('~', realpath($homeDirectory), $path);
+    }
+    /**
+     * Check if access token still valid.
+     */
+    private function checkAccessToken(){
+        $accessToken = json_decode(file_get_contents($this->credentialsPath), true);
+        $this->client->setAccessToken($accessToken);
+        // Refresh the token if it's expired.
+        if ($this->client->isAccessTokenExpired()) {
+            $this->client->fetchAccessTokenWithRefreshToken($this->client->getRefreshToken());
+            file_put_contents($this->credentialsPath, json_encode($this->client->getAccessToken()));
+        }
+    }
+
+    /**
+     * Check if there is internet connection.
+     */
+    private function is_connected()
+    {
+        $connected = @fsockopen("www.google.com", 80);
+        //website, port  (try 80 or 443)
+        if ($connected){
+            $is_conn = true; //action when connected
+            fclose($connected);
+        }else{
+            $is_conn = false; //action in connection failure
+        }
+        return $is_conn;
+    }
+
+
+    /**
+     * Check if there is credential file exists.
+     * @return boolean
+     */
+    public function checkIfCredentialFileExists(){
+        if (file_exists($this->credentialsPath) ) {
+            return true;
+        }
+        return false;
+    }
+}
